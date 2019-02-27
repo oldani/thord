f123f8448e661fd7eb8039dfcf980f60daa73eb7
Ordanis Sanchez <ordanisanchez@gmail.com>
2019-02-26 23:18:32 -0400

    Add QueryArgs, URL to request class, fix scheme typo

--- tests/test_request.py
+++ tests/test_request.py
@@ -1,12 +1,40 @@
 import pytest
 
+from thord.request import QueryArgs, URL
+
+
+def test_queryargs_get():
+    query = "name=test&key=value"
+    args = QueryArgs(query)
+
+    assert args.get("name") == "test"
+    assert args.get("empty") == None
+
+
+def test_queryargs_get_list():
+    query = "name=test1&name=test2&key=value&name=test3"
+    args = QueryArgs(query)
+
+    assert args.get_list("name") == ["test1", "test2", "test3"]
+    assert args.get_list("empty") == []
+
+
+def test_url(scope):
+    query = scope["query_string"].decode()
+
+    url = URL(scope, query)
+
+    assert url.scheme == "http"
+    assert url.path == "/hello/test"
+    assert url.query == "name=test&n=1&n=2"
+
 
 def test_request_method(request):
     assert request.method == "get"
 
 
-def test_request_schema(request):
-    assert request.schema == "http"
+def test_request_scheme(request):
+    assert request.scheme == "http"
 
 
 @pytest.mark.asyncio
--- thord/request.py
+++ thord/request.py
@@ -1,13 +1,58 @@
 from json import loads
-from thord.types import Scope, Receive, AsyncIterable
+from urllib.parse import parse_qs, urlparse
+from thord.types import Scope, Receive, AsyncIterable, Any
+
+
+class QueryArgs(dict):
+    def __init__(self, query_string: str) -> None:
+        """ Parse query string and pass it to dict class. """
+        super().__init__(parse_qs(query_string))
+
+    def get(self, key: str, default=None) -> Any:
+        """ Return first value for a given key. """
+        try:
+            return self[key][0]
+        except KeyError:
+            return default
+
+    def get_list(self, key: str, default=[]) -> list:
+        """ For a given key return a list with all the values
+            associated with it. """
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+
+class URL:
+    def __init__(self, scope: Scope, query_string: str) -> None:
+        path: str = f"{scope['root_path']}{scope['path']}"
+        self._url: str = (
+            f"{scope['scheme']}://{scope['server'][0]}:{scope['server'][1]}"
+            f"{path}?{query_string}"
+        )
+        self.url = urlparse(self._url)
+
+    def __repr__(self) -> str:
+        return f"URL({self._url})"
+
+    def __eq__(self, value) -> bool:
+        return self._url == str(value)
+
+    def __getattr__(self, name: str) -> str:
+        return getattr(self.url, name)
 
 
 class Request:
-    __slots__ = ["__scope", "__receive", "_stream_consumed", "_body"]
+    __slots__ = ["__scope", "__receive", "_stream_consumed", "_body", "args", "url"]
 
     def __init__(self, scope: Scope, receive: Receive) -> None:
         self.__scope = scope
         self.__receive = receive
+
+        query_string: str = scope["query_string"].decode()
+        self.args: QueryArgs = QueryArgs(query_string)
+        self.url: URL = URL(scope, query_string)
 
         self._stream_consumed: bool = False
 
@@ -16,7 +61,7 @@ class Request:
         return self.__scope["method"].lower()
 
     @property
-    def schema(self) -> str:
+    def scheme(self) -> str:
         return self.__scope["scheme"]
 
     @property
